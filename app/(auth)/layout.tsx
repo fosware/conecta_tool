980e851cff9c2cc04fa9bbfd42a152aee65eaca0
fosa <fosahadal@protonmail.com>
2024-12-12 10:41:31 -0600

    auth imports

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,10 @@
-import { ReactNode } from "react";
 import Image from "next/image";
-//import SocialAuthForm from "@/components/forms/SocialAuthForm";
+import { ReactNode } from "react";
+// import SocialAuthForm from "@/components/forms/SocialAuthForm";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-auth-light dark:bg-auth-dark bg-cover bg-no-repeat bg-center px-4 py-10">
+    <main className="flex min-h-screen items-center justify-center bg-auth-light bg-cover bg-center bg-no-repeat px-4 py-10 dark:bg-auth-dark">
       <section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
         <div className="flex items-center gap-2">
           <Image
@@ -12,7 +12,7 @@ const AuthLayout = ({ children }: { children: ReactNode }) => {
             alt="ConnectaTool Logo"
             width={80}
             height={50}
-            className="object-contain invert-colors"
+            className="invert-colors object-contain"
           />
 
           <h1 className="h2-bold text-dark100_light900">
